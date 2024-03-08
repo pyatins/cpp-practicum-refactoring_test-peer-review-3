0b905aebfac076a039b4df0acea697ab3166f60d
pyatins <76805202+pyatins@users.noreply.github.com>
2024-03-08 20:57:36 +0300

    code for peer-review

--- CheckDateTimeValidity.cpp
+++ CheckDateTimeValidity.cpp
@@ -1,46 +1,31 @@
+struct Number {
+    int value = 0;
+    std::string name;
+};
+
+void CheckNumber(const Number& number, int min_value, int max_value) {
+    if (number.value < min_value) {
+        throw std::domain_error(number.name + " is too small"s);
+    }
+    if (number.value > max_value) {
+        throw std::domain_error(number.name + " is too big"s);
+    }
+}
+
+int GetMonthLength(int month, bool is_leap_year) {
+    const std::array month_lengths = {31, 28 + is_leap_year, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    return month_lengths[month - 1];
+}
+
+bool IsLeapYear(int year) {
+    return (year % 4 == 0) && !(year % 100 == 0 && year % 400 != 0);
+}
+
 void CheckDateTimeValidity(const DateTime& dt) {
-    if (dt.year < 1) {
-        throw domain_error("year is too small"s);
-    }
-    if (dt.year > 9999) {
-        throw domain_error("year is too big"s);
-    }
-
-    if (dt.month < 1) {
-        throw domain_error("month is too small"s);
-    }
-    if (dt.month > 12) {
-        throw domain_error("month is too big"s);
-    }
-
-    const bool is_leap_year = (dt.year % 4 == 0) && !(dt.year % 100 == 0 && dt.year % 400 != 0);
-    const array month_lengths = {31, 28 + is_leap_year, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-
-    if (dt.day < 1) {
-        throw domain_error("day is too small"s);
-    }
-    if (dt.day > month_lengths[dt.month - 1]) {
-        throw domain_error("day is too big"s);
-    }
-
-    if (dt.hour < 0) {
-        throw domain_error("hour is too small"s);
-    }
-    if (dt.hour > 23) {
-        throw domain_error("hour is too big"s);
-    }
-
-    if (dt.minute < 0) {
-        throw domain_error("minute is too small"s);
-    }
-    if (dt.minute > 59) {
-        throw domain_error("minute is too big"s);
-    }
-
-    if (dt.second < 0) {
-        throw domain_error("second is too small");
-    }
-    if (dt.second > 59) {
-        throw domain_error("second is too big"s);
-    }
+    CheckNumber({dt.year, "year"s}, 1, 9999);
+    CheckNumber({dt.month, "month"s}, 1, 12);
+    CheckNumber({dt.day, "day"s}, 1, GetMonthLength(dt.month, IsLeapYear(dt.year)));
+    CheckNumber({dt.hour, "hour"s}, 0, 23);
+    CheckNumber({dt.minute, "minute"s}, 0, 59);
+    CheckNumber({dt.second, "second"s}, 0, 59);
 }
